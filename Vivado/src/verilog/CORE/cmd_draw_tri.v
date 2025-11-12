91f004327910f597a03a84fbf5a3290bbc11e9ef
IlanVinograd <ilanv1111@gmail.com>
2025-11-12 18:24:10 +0200

    Refactor cmd_draw_tri module to use named states for better readability; update state handling logic for clarity and maintainability.

--- Vivado/src/verilog/CORE/cmd_draw_tri.v
+++ Vivado/src/verilog/CORE/cmd_draw_tri.v
@@ -29,6 +29,14 @@ module cmd_draw_tri #(
 );
     localparam integer ADDR_W = $clog2(DEPTH);
 
+    localparam [3:0]
+        IDLE  = 0,
+        FETCH = 1,
+        V0_A   = 2, V0_LET  = 3, V0_R = 4,
+        V1_LET = 5, V1_R = 6,
+        V2_LET = 7, V2_R = 8,
+        DONE   = 9;
+
     reg [3:0] stage;
     reg [15:0] edge_addr_lat;
     reg [DW_EDGE-1:0] edge_data_lat;
@@ -52,7 +60,7 @@ module cmd_draw_tri #(
             WE_VERTEX <= 1'b0;
 
             case (stage)
-                4'd0: begin
+                IDLE: begin
                     if (draw_req_pulse && !BUSY) begin
                         BUSY <= 1'b1;
                         edge_addr_lat <= edge_addr;
@@ -61,44 +69,49 @@ module cmd_draw_tri #(
                     end
                 end
 
-                4'd1: begin
+                FETCH: begin
                     edge_data_lat <= edge_data;
                     stage <= 4'd2;
                 end
 
-                4'd2: begin
+                V0_A: begin
                     ADDR_VERTEX <= edge_data_lat[ADDR_W-1:0];
                     stage <= 4'd3;
                 end
-                4'd3: begin
+                V0_LET: begin
                     stage <= 4'd4;
                 end
 
-                4'd4: begin
+                V0_R: begin
                     vertex_data_lat[0] <= vertex_data;
                     ADDR_VERTEX <= edge_data_lat[16 + ADDR_W - 1 -: ADDR_W];
                     stage <= 4'd5;
                 end
-                4'd5: begin
+                V1_LET: begin
                     stage <= 4'd6;
                 end
 
-                4'd6: begin
+                V1_R: begin
                     vertex_data_lat[1] <= vertex_data;
                     ADDR_VERTEX <= edge_data_lat[32 + ADDR_W - 1 -: ADDR_W];
                     stage <= 4'd7;
                 end
-                4'd7: begin
+                V2_LET: begin
                     stage <= 4'd8;
                 end
 
-                4'd8: begin
+                V2_R: begin
                     vertex_data_lat[2] <= vertex_data;
                     stage <= 4'd9;
                 end
 
-                4'd9: begin
+                DONE: begin
                     BUSY  <= 1'b0;
+                    edge_addr_lat <= 16'd0;
+                    edge_data_lat <= {DW_EDGE{1'b0}};
+                    vertex_data_lat[0] <= {DW_VERTEX{1'b0}};
+                    vertex_data_lat[1] <= {DW_VERTEX{1'b0}};
+                    vertex_data_lat[2] <= {DW_VERTEX{1'b0}};
                     stage <= 4'd0;
                 end
 
